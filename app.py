4c4c17e168ef6b0fb621284c795e74ef164e52f0
Luaquinox <160145426+Luaquinox@users.noreply.github.com>
2025-02-18 09:58:19 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
-print ('pereira sabores/n')
+import os
 
-print ('1. Cadastrar restaurante')
-print ('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair')
+def exibir_nome_do_programa():
+ print ('pereira sabores/n')
 
-opcao_escolhida = 
+def exibir_opcoes():
+    print ('1. Cadastrar restaurante')
+    print ('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair')
+
+opcao_escolhida = int(input('Escolha uma opção:'))
+print(f'Você escolheu a opção {opcao_escolhida}\n')
+
+def finalizar_app():
+    os.system('cls')
+    print('Finalizar o app')
+
+if opcao_escolhida == 1:
+   print('Cadastrar lanchonete')
+elif opcao_escolhida == 2:
+     print('Listar lanchonete')
+elif opcao_escolhida == 3:
+     print('Ativar restaurante')
+else:
+    finalizar_app()
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+if __name__ == '__main__':
+    main()
